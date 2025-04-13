e5bf4f8e4b3bc3f94cc13c7690fab319ed6e17f2
hfnu_2010211112 <2942885033@qq.com>
2025-04-13 18:57:18 +0800

    init commit deepseek 环境问题

---
+++ deepseek4j/src/test/java/com/hu/ai/deepseek4j/Deepseek4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu.ai.deepseek4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deepseek4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
